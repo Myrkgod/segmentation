737dc27027bed043d523cb1af7817df21e9d2ebc
Nate <natelharrison@gmail.com>
2022-08-13 22:48:38 -0700

    sliding_window_view working with hardcoded step size

--- data-preprocessing.py
+++ data-preprocessing.py
@@ -9,9 +9,12 @@ from numpy.lib.stride_tricks import sliding_window_view
 mask, header = load(r"C:\Users\natel\Documents\Scan_Iter_0000_CamA_ch0_CAM1_stack0000_488nm_0000000msec_0016966725msecAbs_decon.mha")
 
 print(mask.shape)
-v = sliding_window_view(mask, (30, 30, 30))
+v = sliding_window_view(mask, (100, 100, 100))
+v = v[::90, ::90, ::90, :, :, :]
+
+shape = v.shape
+v = v.reshape((shape[0]*shape[1]*shape[2], shape[3], shape[4], shape[5]))
 print(v.shape)
-# print(v)
 
 
 
